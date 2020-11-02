a949ad04eeed6d201feb750cc53e645876e0588f
msarefin <mohammed.arefin@gmail.com>
2020-11-02 18:59:07 -0500

    Sorting Algorithms completed

--- src/algorithm/UnitTestSorting.java
+++ src/algorithm/UnitTestSorting.java
@@ -8,8 +8,8 @@ public class UnitTestSorting {
       This class is about Unit testing for Sorting Algorithm.
      */
     public static void main(String[] args) {
-        int [] unSortedArray = {6,9,2,5,1,0,4};
-        int [] sortedArray =   {0,1,2,4,5,6,9};
+        int[] unSortedArray = {5, 6, 4, 7, 3, 8, 2, 9, 1, 0, -9, -1, -8, -2, 5, -7, -3, -6, -4, -5};
+        int[] sortedArray = {-9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 5, 6, 7, 8, 9};
         //Create Sort object
         Sort sort = new Sort();
         //apply unsorted array to selectionSort.
@@ -17,14 +17,12 @@ public class UnitTestSorting {
         //verify if the unsorted array is sorted by the selection sort algorithm.
         try {
             Assert.assertEquals(sortedArray, unSortedArray, "Array is not Sorted");
-        }catch(Exception ex){
+//            System.out.println("Sorting Successful");
+        } catch (Exception ex) {
             ex.getMessage();
         }
 
         //Now implement Unit test for rest of the soring algorithm...................below
-
-
-
 
 
     }
