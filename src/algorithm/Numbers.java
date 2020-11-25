e2de2e9bb6f7e6056869ca0a697c5b9e5a1ff4f6
msarefin <mohammed.arefin@gmail.com>
2020-11-25 13:21:18 -0500

    update

--- src/algorithm/Numbers.java
+++ src/algorithm/Numbers.java
@@ -20,7 +20,7 @@ public class Numbers {
 
 	public static void main(String[] args) throws Exception {
 		
-		int [] num = new int[1000000];
+		int [] num = new int[1000];
 		storeRandomNumbers(num);
 		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
 		//Selection Sort
