971717910315032bed0968b154f160beb1880868
morefin <mohammed.arefin@gmail.com>
2020-11-29 13:21:20 -0500

    working with anagram

--- src/string/problems/Anagram.java
+++ src/string/problems/Anagram.java
@@ -6,8 +6,6 @@ public class Anagram {
     public static void main(String[] args) {
         //Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
         //same character but in different order.For example,"CAT" and "ACT", "ARMY" and "MARY".
-
-
-//        Just testing my github
+        
     }
 }
