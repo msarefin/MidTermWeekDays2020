4fff4021e734ec0d391aadfd4686b6505f513787
msarefin <mohammed.arefin@gmail.com>
2020-12-17 14:26:12 -0500

    update

--- src/problemandsolutions/GradeSheet.java
+++ src/problemandsolutions/GradeSheet.java
@@ -19,69 +19,35 @@ public class GradeSheet {
     //
     //Your grade is B.
 
-
-    public int getQuiz() {
-        System.out.println("Quiz Score:");
-        return quiz;
-    }
-
-    public void setQuiz(int quiz) {
-        this.quiz = quiz;
-    }
-
-    public int getMidterm() {
-        System.out.println("Midterm Score:");
-        return midterm;
-    }
-
-    public void setMidterm(int midterm) {
-        this.midterm = midterm;
-    }
-
-    public int getFinals() {
-        System.out.println("Finals Score:");
-        return finals;
-    }
-
-    public void setFinals(int finals) {
-        this.finals = finals;
-    }
-
-    int quiz, midterm, finals;
-
     public static void main(String[] args) {
-
-    }
-    enum grades{
-        A,B,C,F;
-    }
-    public static void grade(){
-        GradeSheet gs = new GradeSheet();
-        int quiz = gs.getQuiz();
-        int midterm = gs.getMidterm();
-        int finals = gs.getFinals();
-        gradeCalculator(AverageScores(quiz, midterm, finals));
+        String st = grade(98.5);
+        System.out.println(st);
     }
 
+   enum Grades{
+       A,B,C,F;
+   }
 
-    public static double AverageScores(int quiz, int midterm, int finals){
-        double av = (quiz+midterm+finals)/3;
-        return av;
-    }
-
-    public static String gradeCalculator(double av) {
-        if (av >= 90) {
-            return grades.A.toString();
-        } else if (av < 90 && av >= 70) {
-            return grades.B.toString();
-        } else if (av < 70 && av >= 50) {
-            return grades.C.toString();
-        } else {
-            return grades.F.toString();
+   public static double averageGrade(double [] marks){
+        double total = 0;
+        for(double m: marks){
+            total = total +m;
         }
-    }
+        double average = total/marks.length;
+        return average;
+   }
 
-
+   public static String grade(double av){
+        if(av>=90){
+            return Grades.A.name();
+        }else if(av>=70 && av<90){
+            return Grades.B.name();
+        }else if(av>=50 && av <70){
+            return Grades.C.name();
+        }else{
+            return Grades.F.name();
+        }
+   }
 
 
 
